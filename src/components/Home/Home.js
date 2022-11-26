19962396c049693af6ed3286f0be4a4ccb660816
lalodiazdiaz <edaurdodiazdiaz93@gmail.com>
2022-11-25 21:49:09 -0600

    fix rules eslint

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,50 +1,75 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import Modal from 'react-modal';
 import Navbar from '../Navbar/Navbar';
 import styles from './Home.module.css';
-import Modal from 'react-modal';
 
 const CUSTOMSTYLES = {
-  content: {
-    width: '70%',
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'white',
-  },
+	content: {
+
+		backgroundColor: 'white',
+		bottom: 'auto',
+		left: '50%',
+		right: 'auto',
+		top: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: '70%',
+	},
 };
 
 function Home() {
+	const [modalIsOpen, setIsOpen] = useState(false);
+	const url = ' https://wa.me/526145153963';
 
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const url = ' https://wa.me/526145153963';
+	const openModal = useCallback(
+		() => {
+			setIsOpen(true);
+		},
+		[],
+	);
 
-  return (
-    <div className={styles.contHome}>
-      <Navbar />
-      <div className={styles.bodyHome}>
-        <div className={styles.agendar} onClick={() => setIsOpen(true)}>Agendar</div>
-      </div>
-      <Modal
-        isOpen={modalIsOpen}
-        style={CUSTOMSTYLES}
-      >
-        <div className={styles.modalCont}>
-          <text>Lic. Claudia Patricia González Moreno</text>
-          <hr />
-          <a href={url} target="_blank" rel="noreferrer">
-            <input type={'submit'} value={'Agendar'} className={styles.inputCita} />
-          </a>
-        </div>
-        <hr />
-        <div className={styles.btnCont}>
-          <button className={styles.btnCloseModal} onClick={() => setIsOpen(false)}>Cerrar</button>
-        </div>
+	const closeModal = useCallback(
+		() => {
+			setIsOpen(false);
+		},
+		[],
+	);
 
-      </Modal>
-    </div>
-  );
+	return (
+		<div className={styles.contHome}>
+			<Navbar />
+			<div className={styles.bodyHome}>
+				<button
+					className={styles.agendar}
+					onClick={openModal}
+					type="button"
+				>Agendar
+				</button>
+			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				style={CUSTOMSTYLES}
+			>
+				<div className={styles.modalCont}>
+					<text>Lic. Claudia Patricia González Moreno</text>
+					<hr />
+					<a href={url} rel="noreferrer" target="_blank">
+						<input className={styles.inputCita} type="submit" value="Agendar" />
+					</a>
+				</div>
+				<hr />
+				<div className={styles.btnCont}>
+					<button
+						className={styles.btnCloseModal}
+						onClick={closeModal}
+						type="button"
+					>
+						Cerrar
+					</button>
+				</div>
+
+			</Modal>
+		</div>
+	);
 }
 
 export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
-import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 function Navbar() {
-  return (
-    <div className={styles.Navbar}>
-      <span className={styles.navLogo}>BEGINNING</span>
-      <div className={styles.navItems}>
-        <Link className={styles.link} to={'login'}>
-          <div className={styles.btnLog}>LogIn</div>
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.Navbar}>
+			<span className={styles.navLogo}>BEGINNING</span>
+			<div className={styles.navItems}>
+				<Link className={styles.link} to="login">
+					<div className={styles.btnLog}>LogIn</div>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;

7db6bc48b3a5335641edcc7cbd10eee61b83ccdb
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-12-12 20:54:41 -0600

    feat: Resolved last login design errors

--- src/services/Login/LoginService.jsx
+++ src/services/Login/LoginService.jsx
@@ -4,12 +4,6 @@ const URL = 'https://emociones-be-production.up.railway.app/api/';
 
 const getLogin = async (body) => {
 	const resp = await axios.post(URL, body);
-	console.log(resp);
-	// if (data.data.error === false) {
-	// 	localStorage.setItem('UserLogged', JSON.stringify(data.data.loginData));
-	// } else if (data.data.error === true) {
-	// }
-	return resp.data;
 };
 
 export default getLogin;

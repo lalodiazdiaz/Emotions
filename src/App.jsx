a3661b280c0703fff47dda36bd53fef21d349456
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2022-12-15 09:26:16 -0600

    fix identation

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import EmergencyModal from './components/EmergencyModal/EmergencyModal';
 
 function App() {
 	return (
-	<BrowserRouter>
-		<Routes>
-			<Route element={<Home />} path="/" />
-			<Route element={<Login />} path="login" />
-			<Route element={<Dashboard />} path="dashboard" />
-		</Routes>
-	</BrowserRouter>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Home />} path="/" />
+				<Route element={<Login />} path="login" />
+				<Route element={<Dashboard />} path="dashboard" />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 

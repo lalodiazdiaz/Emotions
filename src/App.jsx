d0ae135c4b6ab5e33627d2fdc745f32c69a0585b
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-12-19 22:52:02 -0600

    feat: Changes returned to App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home/Home';
 
 function App() {
 	return (

44fc73450092402c8ff1d7495b2d7fe83d3c7b57
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-01-05 16:12:03 -0600

    preparing therapist appointments widget

--- src/components/NextAppointment/NextAppointent.jsx
+++ src/components/NextAppointment/NextAppointent.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import styles from './NextAppointment.module.css';
+import { RANGE } from '../../constants';
 
-function NextAppointment({ date, name, time, title }) {
+function NextAppointment({ action, date, name, time, title }) {
+	const local = JSON.parse(localStorage.getItem('user'));
+	const { range } = local.data;
 	return (
-		<div className={styles.contNextAppointment}>
+		<div className={range === RANGE.patient
+			? styles.contNextAppointment
+			: styles.contNextAppointments}
+		>
 			<div className={styles.contInfomation}>
-				<div className={styles.name}>
+				<div className={range === RANGE.patient
+					? styles.patient
+					: styles.therapist}
+				>
 					<p> {title}: {name}</p>
 				</div>
 				<div className={styles.contDate}>
-					<div className={styles.date}>
+					<div className={range === RANGE.patient
+						? styles.date
+						: styles.dates}
+					>
 						<p>Fecha:</p>
 						<p>{date}</p>
 					</div>
-					<div className={styles.date}>
+					<div className={range === RANGE.patient
+						? styles.date
+						: styles.dates}
+					>
 						<p>Hora:</p>
 						<p>{time}</p>
 					</div>
@@ -21,8 +36,20 @@ function NextAppointment({ date, name, time, title }) {
 
 			</div>
 			<div className={styles.contButtons}>
-				<button className={styles.btnVideocall} type="button">Videollamada</button>
-				<button className={styles.btnCancel} type="button">Cancelar</button>
+				<button
+					className={range === RANGE.patient
+					 ? styles.btnVideocall
+					 : styles.btnVideocalls}
+					type="button"
+				>{action}
+				</button>
+				<button
+					className={range === RANGE.patient
+					 ? styles.btnCancel
+					 : styles.btnsCancel}
+					type="button"
+				>Cancelar
+				</button>
 			</div>
 		</div>
 	);
--- src/components/Psychologist/Dates/Dates.jsx
+++ src/components/Psychologist/Dates/Dates.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from './Dates.module.css';
+import NextAppontment from '../../NextAppointment/NextAppointent';
 
 function Dates() {
 	return (
 		<div className={styles.contAppointments}>
 			<div className={styles.Appointments}>
-				<div className={styles.nextAppointments}>
-					<h1>Citas</h1>
+				<div className={styles.contNextAppointment}>
+					<h1>Proximas Citas</h1>
+					<div className={styles.nextAppointments}>
+						<NextAppontment
+							action="Iniciar"
+							date="21/3/2023"
+							name="Eduardo Diaz Diaz de  leon"
+							time="14:00:00"
+							title="Paciente"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>

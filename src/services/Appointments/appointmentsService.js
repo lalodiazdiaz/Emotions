6e965e228d3b31fa742b96e1cd5caf3638dbcba9
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-01-19 12:28:42 -0600

    change localStorage

--- src/services/Appointments/appointmentsService.js
+++ src/services/Appointments/appointmentsService.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
 const API_URL = `${process.env.REACT_APP_API_BASE_URL}`;
+const local = JSON.parse(localStorage.getItem('user'));
+let authStr = '';
+
+if (local) {
+	authStr = `Bearer ${local.data.token}`;
+}
 
 export const getNextAppointment = async (params) => {
-	const local = JSON.parse(localStorage.getItem('user'));
-	const { token } = local.data;
-	const AuthStr = `Bearer ${token}`;
-	const res = await axios.get(API_URL + params, { headers: { Authorization: AuthStr } });
+	const res = await axios.get(API_URL + params, { headers: { Authorization: authStr } });
 	return res;
 };
 
 export const deleteAppointment = async ({ id }) => {
-	const local = JSON.parse(localStorage.getItem('user'));
-	const { token } = local.data;
-	const AuthStr = `Bearer ${token}`;
 	const res = await axios.delete(`${API_URL}/appointments`, {
 		data: { _id: id },
-		headers: { Authorization: AuthStr },
+		headers: { Authorization: authStr },
 	});
 	return res;
 };
--- src/services/Logout/LogoutService.js
+++ src/services/Logout/LogoutService.js
@@ -2,13 +2,17 @@ import axios from 'axios';
 
 const API_URL = `${process.env.REACT_APP_API_BASE_URL}`;
 
+const local = JSON.parse(localStorage.getItem('user'));
+let authStr = '';
+
+if (local) {
+	authStr = `Bearer ${local.data.token}`;
+}
+
 const logOutSession = async () => {
-	const local = JSON.parse(localStorage.getItem('user'));
-	const { token } = local.data;
-	const AuthStr = `Bearer ${token}`;
 	const res = await axios.delete(
 		`${API_URL}/session/logout`,
-		{ headers: { Authorization: AuthStr } },
+		{ headers: { Authorization: authStr } },
 	);
 	localStorage.removeItem('user');
 	return res.data;

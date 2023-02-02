0076f20365a94f1624a527391759a46d2296f2ab
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-02-02 15:42:56 -0600

    update styles

--- src/components/Psychologist/Patients/Patients.jsx
+++ src/components/Psychologist/Patients/Patients.jsx
@@ -31,11 +31,9 @@ function Patients() {
 		return (
 			<div className={styles.contPatients}>
 				<div className={styles.Patients}>
-					<div className={styles.contNextPatients}>
-						<h1>Pacientes</h1>
-						<div className={styles.loaderContainer}>
-							<Loader />
-						</div>
+					<h1>Pacientes</h1>
+					<div className={styles.loaderContainer}>
+						<Loader />
 					</div>
 				</div>
 			</div>
@@ -44,38 +42,42 @@ function Patients() {
 	return (
 		<div className={styles.contPatients}>
 			<div className={styles.Patients}>
-				<div className={styles.contNextPatients}>
-					<h1>Pacientes</h1>
-					{patient.length >= 1
-						? (
-							<div className={styles.gridNextPatients}>
-								{patient.map((item) => (
-									<div key={item.id} className={styles.notPatients}>
-										<p>Paciente: {item.fullName}</p>
+				<h1>Pacientes</h1>
+				{patient.length >= 1
+					? (
+						<div className={styles.gridNextPatients}>
+							{patient.map((item) => (
+								<div className={styles.contCard}>
+									<div className={styles.contInformation}>
+										<div className={styles.dataCard}>
+											<p>Psicologo: {item.fullName}</p>
+										</div>
+									</div>
+									<div className={styles.contButtons}>
 										<button
 											className={styles.btnDetails}
-											onClick={navigatePatients}
+											onClick={navigateScreen}
 											type="button"
 										>
 											Detalles
 										</button>
 									</div>
-								))}
-							</div>
-						)
-						: (
-							<div className={styles.notPatients}>
-								<p>No tienes pacientes registrados.</p>
-							</div>
-						)}
-					<button
-						className={styles.btnAdd}
-						onClick={navigateScreen}
-						type="button"
-					>
-						Agregar
-					</button>
-				</div>
+								</div>
+							))}
+						</div>
+					)
+					: (
+						<div className={styles.notPatients}>
+							<p>No tienes pacientes registrados.</p>
+						</div>
+					)}
+				<button
+					className={styles.btnAdd}
+					onClick={navigateScreen}
+					type="button"
+				>
+					Agregar
+				</button>
 			</div>
 		</div>
 	);
